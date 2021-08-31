0e774284bea6032bd2c57272d268872f83f7a532
Nat <nathalie.piat1@gmail.com>
2021-08-31 17:49:05 +0200

    Updates

---
+++ precache-manifest.fc596f9095face30eacc0af3af5c01b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a47a3c9588896b15053c44d63bebbeb1",
+    "url": "/emoji-garden/index.html"
+  },
+  {
+    "revision": "a465b6cc08da13c1d66c",
+    "url": "/emoji-garden/static/css/main.8bc6e8fa.chunk.css"
+  },
+  {
+    "revision": "3252affe30741c9f048b",
+    "url": "/emoji-garden/static/js/2.a61521a3.chunk.js"
+  },
+  {
+    "revision": "a465b6cc08da13c1d66c",
+    "url": "/emoji-garden/static/js/main.832870b4.chunk.js"
+  },
+  {
+    "revision": "ea3e0a6a9f969ce9dc6d",
+    "url": "/emoji-garden/static/js/runtime~main.45c1c619.js"
+  }
+]);
